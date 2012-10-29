0011e00c4dbff7e505a9e0f03bba98431f7fc83a
Jamie Brandon <jamie@scattered-thoughts.net>
2012-10-29 12:49:32 -0400

    Init

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject droplet "0.1.0-SNAPSHOT"
+  :description "Datalog with time"
+  :url "http://github.com/jamii/droplet"
+  :license {:name "LGPL"
+            :url "http://www.gnu.org/licenses/lgpl.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [slingshot "0.10.3"]
+                 [strucjure "0.3.0"]])
