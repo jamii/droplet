5effc4eb99c28cb41c92b6d57c17f63acba864d1
Leo Franchi <lfranchi@kde.org>
2012-11-27 12:21:30 -0500

    Remove unneeded cart lattice

--- src/droplet/examples/cart.clj
+++ src/droplet/examples/cart.clj
@@ -8,20 +8,6 @@
 (def ACTION_OP 0)
 (def CHECKOUT_OP 1)
 
-;; Cart lattice has an actions map {op-id #{[actions]}} and an output checkout map {item-id count}
-;; First item in [vals] is either ACTION_OP or CHECKOUT_OP, rest are args to the operation
-;; The lattice has a monotone is-complete function, returns when a checkout has happened
-(defrecord CartLattice [actions]
-  SemiLattice
-  (bottom [this]
-    (->CartLattice {}))
-  (lte? [this that]
-    ;; what does lte? mean in this case?
-    (lte? (:actions this) (:actions that)))
-  (join [this that]
-    (->CartLattice (join (:actions this) (:actions that)))))
-
-
 (def cart-states
   {:sessions {}
    :add #{}
@@ -31,7 +17,7 @@
 (defn cart-summary
   [sessions userid from-op to-op]
   (apply merge-with + {}
-      (let [actions (:actions (get sessions userid))
+      (let [actions (get sessions userid)
             action-ids (sort (keys actions))]
         (if (and (= CHECKOUT_OP (nth (first (get actions to-op)) 0))
                  (every? #(some (fn [x] (= %1 x)) action-ids) (range from-op to-op))) ;; All ids in the range are in the list of actions
@@ -44,9 +30,9 @@
   [(persistent :sessions)
    (ephemeral :add)
    (ephemeral :checkout)
-   (d/deduct :sessions [userid (->CartLattice {opid #{[ACTION_OP item cnt]}})]
+   (d/deduct :sessions [userid {opid #{[ACTION_OP item cnt]}}]
              :add {:userid ?userid :opid ?opid :item ?item :cnt ?cnt})
-   (d/deduct :sessions [userid (->CartLattice {opid #{[CHECKOUT_OP from-op]}})]
+   (d/deduct :sessions [userid {opid #{[CHECKOUT_OP from-op]}}]
              :checkout {:userid ?userid :opid ?opid :from-op ?from-op})])
 
 (defn add-to-cart! [carts userid opid item cnt]
@@ -68,12 +54,18 @@
     (add-to-cart! c 999 3 "absinthe" 9)
     (add-to-cart! c 999 4 "beer" 1)
     (add-to-cart! c 111 5 "cookies" 17)
+    (add-to-cart! c 111 6 "cookies" -3)
+    (add-to-cart! c 111 7 "cookies" -1)
     (let [result (checkout! c 999 5 1)]
       (if-not (= (get result "beer") 6)
         (throw (agent-error c)))
       (if-not (= (get result "gin") 3)
         (throw (agent-error c)))
       (if-not (= (get result "absinthe") 9)
+        (throw (agent-error c))))
+    (let [result (checkout! c 111 8 5)]
+      (if-not (= (get result "cookies") 13)
         (throw (agent-error c))))))
-
+;
+;
 ; (:sessions (:states @c))
