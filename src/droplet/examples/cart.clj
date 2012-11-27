c9585aaccae6e00f6a2c3033594bbe122e8842cc
Leo Franchi <lfranchi@kde.org>
2012-11-27 14:31:58 -0500

    Rewrite checkout! and change cart format to make it cleaner

--- src/droplet/examples/cart.clj
+++ src/droplet/examples/cart.clj
@@ -8,31 +8,40 @@
 (def ACTION_OP 0)
 (def CHECKOUT_OP 1)
 
+;; A lattice that's not a true lattice---values
+;; are simply copied over if there is a key conflict
+;; TODO should use e.g. vector clocks for opid
+(defrecord Cart [actions]
+  SemiLattice
+  (bottom [this]
+    (->Cart {}))
+  (lte? [this that]
+    (lte? (:actions this) (:actions that)))
+  (join [this that]
+    (->Cart (merge-with (fn [one two] two) (:actions this) (:actions that)))))
+
 (def cart-states
   {:sessions {}
    :add #{}
    :checkout #{}})
 
-;; Monotonic (once a checkout action has been sent, this always returns the same value. until it has, returns nil)
 (defn cart-summary
   [sessions userid from-op to-op]
+  (if-not (and ;; Make sure the desired to-op is a CHECKOUT action, and that we have all ids in the range
+            (= CHECKOUT_OP (get-in sessions [userid :actions to-op :op]))
+            (every? #(some (fn [x] (= %1 x)) (keys (get-in sessions [userid :actions]))) (range from-op to-op)))
+    (throw (Exception. "Tried to call checkout but missing ids in range, or missing CHECKOUT_OP at id")))
   (apply merge-with + {}
-      (let [actions (get sessions userid)
-            action-ids (sort (keys actions))]
-        (if (and (= CHECKOUT_OP (nth (first (get actions to-op)) 0))
-                 (every? #(some (fn [x] (= %1 x)) action-ids) (range from-op to-op))) ;; All ids in the range are in the list of actions
-          (for [id (range from-op to-op)]
-              {(nth (first (get actions id)) 1) (nth (first (get actions id)) 2)})
-          (throw (Exception. "Tried to call checkout but missing ids in range, or missing CHECKOUT_OP at id")))
-        )))
+    (for [opid (range from-op to-op)]
+      {(get-in sessions [userid :actions opid :item]) (get-in sessions [userid :actions opid :cnt])})))
 
 (def cart-rules
   [(persistent :sessions)
    (ephemeral :add)
    (ephemeral :checkout)
-   (d/deduct :sessions [userid {opid #{[ACTION_OP item cnt]}}]
+   (d/deduct :sessions [userid (->Cart {opid {:op ACTION_OP :item item :cnt cnt}})]
              :add {:userid ?userid :opid ?opid :item ?item :cnt ?cnt})
-   (d/deduct :sessions [userid {opid #{[CHECKOUT_OP from-op]}}]
+   (d/deduct :sessions [userid (->Cart {opid {:op CHECKOUT_OP :from-op from-op}})]
              :checkout {:userid ?userid :opid ?opid :from-op ?from-op})])
 
 (defn add-to-cart! [carts userid opid item cnt]
